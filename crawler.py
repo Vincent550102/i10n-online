c1e894f3daa54dec14711a23e93529c89526f1eb
Vincent550102 <Vincent550102@users.noreply.github.com>
2024-04-04 10:53:42 +0000

    deploy: 7567de5caa21930674a39f7aba5777c2e9c7739f

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import json
 r = requests.get("https://hackmd.io/@l10n-tw/glossaries", headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'})
 temps = re.findall(r"\|(.*?)\|(.*?)\|(.*?)\|(.*?)\|", r.text)[3:]
 datas = []
-for temp in temps:
+for temp in temps[:5]:
     parts = re.split(r'&lt;br&gt;|/|（.*?）', temp[2])
     parts = [part.strip() for part in parts if parts]
     for part in parts:
@@ -18,9 +18,9 @@ for temp in temps:
             except:
                 pass
         datas.append([
-            temp[0].split("&lt;br&gt;")[0],
-            temp[1].strip(),
-            r['data']['text'],
+            temp[0].strip(),
+            temp[1].split(r"&lt;br&gt;")[0].strip(),
+            r['data']['text'].strip(),
             temp[3].strip()
         ])
 
